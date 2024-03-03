5d755f8f73bafe245ed0b4710b10e0b0b79a8aee
Jayesh Nalwa <nalwajayesh97@gmail.com>
2024-03-04 00:28:19 +0530

    wrote acheivement controller

--- controllers/acheivementController.js
+++ controllers/acheivementController.js
@@ -1,4 +1,19 @@
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const AchievementModel = require("../models/Achievements");
 
+module.exports = {
+    createAcheivement: async (req, res) => {
+        const newAcheivement = new AchievementModel(req.body);
+
+        try{
+            const savedAcheivement = await newAcheivement.save();
+            const { __v, createdAt, updatedAt, ...newAcheivementInfo } = savedAcheivement._doc;
+
+            res.status(200).json(newAcheivementInfo);
+        } catch(error) {
+            res.status(500).json(error);
+        }
+    }
+}
