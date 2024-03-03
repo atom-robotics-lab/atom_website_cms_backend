8cab635fd32fbedace9f36d210201ac2a7005da1
Jayesh Nalwa <nalwajayesh97@gmail.com>
2024-03-04 00:06:47 +0530

    wrote auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,3 +2,34 @@ const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
+module.exports = {
+    createUser: async (req, res) => {
+        const { username, password } = req.body;
+        try {
+            const userDoc = await User.create({
+            username,
+            password: bcrypt.hashSync(password, salt),
+            });
+            res.status(200).json({ userDoc });
+        } catch (error) {
+            console.log(error.message);
+            res.status(400).json(error);
+        }
+    },
+    loginUser: async (req, res) => {
+        const { username, password } = req.body;
+        const userDoc = await User.findOne({ username });
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie("token", token).json({
+                id: userDoc._id,
+                username,
+            });
+            });
+        } else {
+            res.status(400).json("wrong credentials");
+        }
+    }
+}
