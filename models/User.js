e282250819dee4fa30940c5ae0f33493ce2efa22
prakhar1535 <prakharsharma1535@gmail.com>
2024-03-04 00:14:19 +0530

    added imports

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const User = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
 });
 
 const UserModel = mongoose.model("User", User);
